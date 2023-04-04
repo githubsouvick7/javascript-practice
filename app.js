8c4d6a7e1f2530703a9c6d1e5a8153e5cd5cde15
Souvick Chakraborty <hellosouvickk@gmail.com>
2023-04-04 17:55:35 +0530

    first program

---
+++ app.js
@@ -0,0 +1,52 @@
+// console.log("hello world")
+
+//there are 6 premetive data type
+// 1: undefined;
+// 2: Number;
+// 3: String;
+// 4: BigInt;
+// 5: Symbol;
+// 6: Boolean;
+// 7: Null;
+
+// veriable are in java script is var let and const;
+
+// var name = "Souvick";
+// console.log(name)
+
+// The var statement declares a variable.but its not good practice alaways use let or const
+
+// let a = 45;
+// let b = 45;
+
+// let c = a + b;
+// console.log(c);
+
+// Null vs Undefined-----
+
+// let a = 'Souvick'
+// let b = 10;
+
+// console.log(a + b)
+// // use of typeof
+// console.log(typeof(a+b))
+
+// // use of const object
+
+// const n = {
+//     name:'souvick',
+//     age: 22,
+// }
+
+// n = 'chakraborty'
+// console.log(n)
+
+// add a new things in object-->
+
+// n['name'] = "Souvick",
+// n['surname'] = "Chakraborty"
+// console.log(n)
+
+// how to access object with name-->
+// console.log(n.name);
+// console.log(n['name']);
