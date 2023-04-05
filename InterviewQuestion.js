a0b96a8516357fcc990eaac5c47d274a3dec6be3
Souvick Chakraborty <hellosouvickk@gmail.com>
2023-04-05 16:50:46 +0530

    Interview Question part-2

---
+++ InterviewQuestion.js
@@ -0,0 +1,61 @@
+
+// function add(a) {
+//     return function (b) {
+//         return function () {
+//             return a - b;
+//         }
+//     }
+// }
+
+// let res = add(2);
+// let res2 = res(4);
+// let res3 = res2();
+// console.log(res3);
+
+// console.log(add(2)(4)());
+
+
+
+
+// let a = 10;
+// function x() {
+//     var a = 20;
+//     function y() {
+//         console.log(a);
+//     }
+//     y();
+// }
+
+// x();
+
+// console.log("1");
+// setTimeout(() => console.log("2"), 0);
+// Promise.resolve(() => console.log("3")).then(res => res());
+// console.log("4");
+
+
+// 1
+// 4
+// 3
+// 2
+
+
+// console.log(typeof null);
+// console.log(typeof undefined);
+
+
+// callback
+
+
+
+// let a = 10;
+// function x() {
+//     var a = 20;
+//     function y() {
+//         console.log(a);
+//     }
+//     y();
+// }
+// x();
+
+
